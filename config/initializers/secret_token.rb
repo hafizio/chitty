8bdfcbcb9087567c9eb3c6891dabbacd325eea35
ibnukamy <ibnukamy@gmail.com>
2013-09-13 10:30:53 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chitty::Application.config.secret_key_base = '82307301e6cd488c226df2c68847fa57c9ccf1b287b3ed033b179f6a7c5ad7eeec05c31cba55f484104e9416ff94912ae9a1d33cdcee56c03afcc003c89d04bd'
